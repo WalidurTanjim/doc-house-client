055d930029123bc1f45ad111b39d36e54ef715d6
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-09-29 00:39:10 +0600

    SectionTitle page updated

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const SectionTitle = ({ title, subTitle }) => {
     return (
         <div className='w-full md:w-[600px] mx-auto mb-10 text-center'>
-            <h1 className='text-2xl md:text-3xl text-slate-900 font-medium mb-2'>{title}</h1>
-            <p className='text-gray-600'>{subTitle}</p>
+            <h1 className='text-2xl md:text-3xl text-slate-900 font-medium mb-2'>{title ? title : undefined}</h1>
+            <p className='text-gray-600'>{subTitle ? subTitle : undefined}</p>
         </div>
     );
 };
