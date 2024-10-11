01ed397305306ba28c716d56d6e2101c0f5cd9e6
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-10-11 23:15:13 +0600

    PrivateRoute created

---
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    if(loading){
+        return (
+            <div className='w-full py-20 text-center'>
+                <h1 className='text-2xl font-medium text-center text-slate-800'>Loading...</h1>
+            </div>
+        )
+    }
+
+    if(user){
+        return children;
+    }
+    
+    return <Navigate to="/signIn" state={{ from: location}} replace={true}></Navigate>
+};
+
+export default PrivateRoute;
--- src/pages/Dashboard/AdminDashboard/AddDoctor/AddDoctor.jsx
+++ src/pages/Dashboard/AdminDashboard/AddDoctor/AddDoctor.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useForm } from "react-hook-form"
 import DashboardRoutes from '../../../../components/DashboardRoutes/DashboardRoutes';
 
 const AddDoctor = () => {
+    const { register, handleSubmit, reset, watch, formState: { errors } } = useForm()
+    
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+
     return (
         <div className="profileInfoContainer container mx-auto px-6 my-16">
             <DashboardRoutes></DashboardRoutes>
 
-            <h1>Add Doctor</h1>
+            <h2 className="text-lg font-medium text-gray-800 dark:text-white">Add New Doctor</h2>
+
+            {/* form div starts */}
+            <div className='my-10 w-full'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h1>Form</h1>
+
+                    <button className='py-2 w-full rounded-md text-center font-medium text-white border border-[#F7A582] bg-[#F7A582] hover:bg-[#f7824f] active:bg-[#F7A582] shadow-sm text-sm mt-2'>Add Doctor</button>
+                </form>
+            </div>
         </div>
     );
 };
