c2b11165948b180fd70c1ef7750d63c7b82dc90e
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-11-30 07:20:46 +0600

    Added signedin user profile image to Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, BellIcon, XMarkIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import useAuth from '../../hooks/useAuth';
@@ -88,7 +88,11 @@ const Header = () => {
                                         <MenuButton className="relative flex rounded-full bg-[#143D3A] text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                             <span className="absolute -inset-1.5" />
                                             <span className="sr-only">Open user menu</span>
-                                            <img alt="" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-8 w-8 rounded-full" />
+                                            {
+                                                user?.photoURL ?
+                                                <img alt="" src={user?.photoURL} className="h-8 w-8 rounded-full" /> :
+                                                <UserCircleIcon className="h-8 w-8 text-white"></UserCircleIcon>
+                                            }
                                         </MenuButton>
                                     </div>
                                     
@@ -134,7 +138,11 @@ const Header = () => {
                                         <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                             <span className="absolute -inset-1.5" />
                                             <span className="sr-only">Open user menu</span>
-                                            <img alt="" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" className="h-10 w-10 rounded-full" />
+                                            {
+                                                user?.photoURL ?
+                                                <img alt="" src={user?.photoURL} className="h-8 w-8 rounded-full" /> :
+                                                <UserCircleIcon className="h-8 w-8 text-white"></UserCircleIcon>
+                                            }
                                         </MenuButton>
                                     </div>
                                 </Menu>
