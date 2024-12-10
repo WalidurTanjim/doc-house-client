eba9460c3a2841c389d732a570066ab888cd362a
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-10 23:21:09 +0600

    User delete confirmation btn added

--- src/pages/Dashboard/AdminDashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AdminDashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import DashboardRoutes from '../../../../components/DashboardRoutes/DashboardRoutes';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 
 const AllUsers = () => {
     const [users, setUsers] = useState([]);
 
     const loadedData = useLoaderData();
+    const axiosSecure = useAxiosSecure();
+
+    
     useEffect(() => {
         if(loadedData.length > 0){
             setUsers(loadedData);
@@ -14,7 +19,31 @@ const AllUsers = () => {
         }
     }, [loadedData, setUsers])
     // console.log(loadedData, users);
-    
+
+    // deleteHandler
+    const deleteHandler = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // axiosSecure.delete(`/users/${id}`)
+
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+                console.log(id);
+            }
+        });
+    }
+
     return (
         <div className="allUsers container mx-auto px-6 my-16">
             <DashboardRoutes></DashboardRoutes>
@@ -65,7 +94,7 @@ const AllUsers = () => {
                                                         </td>
                                                         <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                             <div className="flex items-center justify-end gap-x-6">
-                                                                <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                                                                <button className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none" onClick={() => deleteHandler(user?._id)}>
                                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                                     </svg>
